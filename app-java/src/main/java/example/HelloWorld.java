db43012008fba9cd38d7b01baccd53d0d82ad80d
usui <ma-usui@foresight.co.jp>
2017-07-14 21:31:43 +0900

    commit

--- app-java/src/main/java/example/HelloWorld.java
+++ app-java/src/main/java/example/HelloWorld.java
@@ -4,7 +4,13 @@ public class HelloWorld {
 
   public void coveredByUnitTest() {
     System.out.println("coveredByUnitTest1");
-    System.out.println("coveredByUnitTest2");
+    for (int i=0; i< 10 ;i++) {
+    if (i%2==0) {
+    	System.out.println("iは"+ "偶数です(" + i + "")");
+    }else{
+    			System.out.println("iは"+ "奇数です(" + i + "")");
+    }
+    }
   }
 
   public void coveredByIntegrationTest() {
