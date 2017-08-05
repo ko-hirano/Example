eede88d67a3df891945ed237da215b9bac139d7c
usui <ma-usui@foresight.co.jp>
2017-08-05 16:20:39 +0900

    コミット

--- src/main/java/example/HelloWorld.java
+++ src/main/java/example/HelloWorld.java
@@ -5,14 +5,14 @@ import org.apache.log4j.Logger;
 
 
 public class HelloWorld {
+	  private String sample;
   Logger logger = Logger.getLogger(Logger.class.getName ());
 
   public void coveredByUnitTest() {
-    System.out.println("coveredByUnitTest1開始");
+		logger.info("coveredByUnitTest1開始");
     for (int i=0; i< 11 ;i++) {
 	    if (i%2==0) {
 	    	logger.info("i(" + i + ")は偶数です");
-	        System.out.println("i(" + i + ")は偶数です");
 	    	switch(i) {
 	    	case 2:logger.info( "iは2です");
 	    	case 4:logger.info( "iは4です");
@@ -23,7 +23,6 @@ public class HelloWorld {
 	    	}
 	    }else{
 	    	logger.info("i(" + i + ")は奇数です");
-	        System.out.println("i(" + i + ")は奇数です");
 	    	switch(i) {
 	    	case 1:logger.info( "iは1です");
 	    	case 3:logger.info( "iは3です");
@@ -34,7 +33,7 @@ public class HelloWorld {
 	    	}
 	    }
     }
-    System.out.println("coveredByUnitTest1終了");
+	logger.info("coveredByUnitTest1終了");
   }
 
   public void notCovered() {
--- src/main/java/example/TestDuplications.java
+++ src/main/java/example/TestDuplications.java
@@ -8,37 +8,44 @@ public class TestDuplications {
   Logger logger = Logger.getLogger(Logger.class.getName ());
 
   public void testDuplications() {
-    System.out.println("coveredByUnitTest1開始");
-    for (int i=0; i< 11 ;i++) {
+	logger.info("coveredByUnitTest1開始");
+    for (int i=0; i< 10 ;i++) {
 	if (i%2==0) {
 	logger.info("i(" + i + ")は偶数です");
-    System.out.println("i(" + i + ")は偶数です");
 	switch(i) {
-	case 2:logger.info( "iは2です");
-	case 4:logger.info( "iは4です");
-	case 6:logger.info( "iは6です");
-	case 8:logger.info( "iは8です");
-	case 10:logger.info( "iは10です");
-	default:break;
+	case 2:
+		logger.info( "iは2です");
+	case 4:
+		logger.info( "iは4です");
+	case 6:
+		logger.info( "iは6です");
+	case 8:
+		logger.info( "iは8です");
+	case 10:
+		logger.info( "iは10です");
+	default:
 	}
 	}else{
 	logger.info("i(" + i + ")は奇数です");
-    System.out.println("i(" + i + ")は奇数です");
 	switch(i) {
-	case 1:logger.info( "iは1です");
-	case 3:logger.info( "iは3です");
-	case 5:logger.info( "iは5です");
-	case 7:logger.info( "iは7です");
-	case 9:logger.info( "iは9です");
+	case 1:
+		logger.info( "iは1です");
+	case 3:
+		logger.info( "iは3です");
+	case 5:
+		logger.info( "iは5です");
+	case 7:
+		logger.info( "iは7です");
+	case 9:
+		logger.info( "iは9です");
 	default:break;
 	}
 	    }
     }
-    System.out.println("coveredByUnitTest1終了");
+	logger.info("coveredByUnitTest1終了");
   }
 
   public void notCovered() {
-	logger.info("notCovered");
     System.out.println("notCovered");
   }
 
